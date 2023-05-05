606fc319f6166e6b5214924bba80436267f5e551
IgorNCosta14 <igorncosta14@hotmail.com>
2023-05-05 01:40:45 -0300

    feat: cors

--- application.py
+++ application.py
@@ -5,10 +5,12 @@ from flask import Flask
 from dotenv import load_dotenv
 from routes import routes
 from extentions import database
+from flask_cors import CORS
 
 load_dotenv()
 
 application = Flask(__name__)
+CORS(application, resources={r"*": {"origins": "*"}})
 application.config["MONGO_URI"] = os.environ.get('DB_CONNECT')
 application.secret_key = os.environ.get('FLASK_SECRET_KEY')
 application.register_blueprint(routes)
