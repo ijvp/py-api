e9180e256e053eabc7fdd65fecb72a2e35077262
Igor Nascimento Costa <igorncosta14@hotmail.com>
2023-06-02 18:27:57 -0300

    Update database.py

--- extentions/database.py
+++ extentions/database.py
@@ -1,8 +1,15 @@
-from flask_pymongo import PyMongo
-from pymongo.errors import ConnectionFailure
-import certifi
+# from flask_pymongo import PyMongo
+# from pymongo.errors import ConnectionFailure
+# import certifi
 
-mongo = PyMongo()
+# mongo = PyMongo()
 
-def init_app(app):
-    mongo.init_app(app, tlsCAFile=certifi.where())
+# def init_app(app):
+#     app.config["MONGO_TLS_CA_FILE"] = certifi.where()
+#     mongo.init_app(app)
+
+from flask import Flask
+from extentions.database import mongo
+
+app = Flask(__name__)
+mongo.init_app(app)
