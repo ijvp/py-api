eea448b3d5ea6e2a480aa34c08a810d28ea5fc59
Igor Nascimento Costa <igorncosta14@hotmail.com>
2023-07-25 18:06:33 -0300

    update: git actions

--- application.py
+++ application.py
@@ -15,11 +15,13 @@ load_dotenv()
 application = Flask(__name__)
 
 CORS(application, resources={r"*": {"origins": "*"}})
-# application.secret_key = os.environ.get('FLASK_SECRET_KEY')
-application.secret_key = "testesail"
+application.secret_key = os.environ.get('FLASK_SECRET_KEY')
 application.register_blueprint(google_ads_bp)
 application.register_blueprint(google_analytics_bp)
 application.logger.addHandler(logging.StreamHandler(sys.stdout))
 
 if __name__ == '__main__':
-  application.run(application.run(host='127.0.0.1', port=os.environ.get('PORT'), debug=os.environ.get('DEBUG')))
+  if os.environ.get('ENV') == 'development':
+    application.run(host=os.environ.get('HOST'), port=os.environ.get('PORT'), debug=os.environ.get('DEBUG'))
+  else:
+    application.run(host=os.environ.get('HOST'), port=os.environ.get('PORT'), debug=os.environ.get('DEBUG'), server='gunicorn')
