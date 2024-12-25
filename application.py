ef83a10d15e55fa43ddcb2d4da49c4121bd1fbd5
pedrofernandes <pedrofernandes@cloud.upwork.com>
2024-12-24 23:45:02 -0300

    we're so back

--- application.py
+++ application.py
@@ -3,8 +3,8 @@
 import os
 from flask import Flask
 from dotenv import load_dotenv
-from app.routes.ads import google_ads_bp
-from app.routes.analytics import google_analytics_bp
+# from app.routes.ads import google_ads_bp
+# from app.routes.analytics import google_analytics_bp
 from extensions import database
 from flask_cors import CORS
 import logging
@@ -15,9 +15,9 @@ load_dotenv()
 application = Flask(__name__)
 
 CORS(application, resources={r"*": {"origins": "*"}})
-application.secret_key = os.environ.get('FLASK_SECRET_KEY')
-application.register_blueprint(google_ads_bp)
-application.register_blueprint(google_analytics_bp)
+# application.secret_key = os.environ.get('FLASK_SECRET_KEY')
+# application.register_blueprint(google_ads_bp)
+# application.register_blueprint(google_analytics_bp)
 application.logger.addHandler(logging.StreamHandler(sys.stdout))
 
 if __name__ == '__main__':
